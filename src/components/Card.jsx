d15e802a0e7088db5dff363ad944b1f2eb4d872f
Mohammad Naeem <binnaeem96@gmail.com>
2026-06-01 02:38:00 -0400

    add creator creation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
-function Card({ name, url, description, imageURL }) {
+import { Link } from 'react-router-dom'
+
+
+function Card({ id, name, url, description, imageURL }) {
     return (
     <div className="card">
         {imageURL && <img src={imageURL} alt={name} />}
         <h2>{name}</h2>
         <a href={url} target="_blank" rel="noopener noreferrer">{url}</a>
         <p>{description}</p>
-        
+
+        <Link to={`/edit/${id}`}><button>Edit</button></Link>
+        <Link to={`/${id}`}><button>View</button></Link>
     </div>
     )
 }
--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { supabase } from "../client"
 import Card from "../components/Card"
 import { useState } from "react"
@@ -17,9 +18,15 @@ function ShowCreators() {
       
   }, [])
 
-  return creators.map(creator => (
-    <Card key={creator.id} {...creator} />
-  ))
+  return (
+    <div>
+      {<Link to="/new"><button>New Creator</button></Link>}
+      {creators.map(creator => (
+        <Card key={creator.id} {...creator} />
+      ))}
+    </div>
+    
+)
 }
 
 export default ShowCreators
