52d119a46f9f6348b6d699d7b903132493b264fd
Mohammad Naeem <binnaeem96@gmail.com>
2026-06-01 01:14:56 -0400

    add page

--- src/pages/AddCreator.jsx
+++ src/pages/AddCreator.jsx
@@ -1,5 +1,26 @@
+import { supabase } from "../client"
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+
 function AddCreator() {
-  return <h1>Add Creator</h1>
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [url, setUrl] = useState('')
+  const navigate = useNavigate()
+
+  async function handleNew() {
+    await supabase.from('creators').insert({ name, description, url })
+    navigate('/')
+    
+  }
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+      <input value={url} onChange={(e) => setUrl(e.target.value)} />
+      <button onClick={handleNew}>Submit</button>
+    </div>
+  )
 }
 
 export default AddCreator
