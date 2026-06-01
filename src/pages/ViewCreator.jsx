c8766ed261bee4441c74b668f49f40b6b175d477
Mohammad Naeem <binnaeem96@gmail.com>
2026-05-31 21:37:35 -0400

    detail page

--- src/pages/ViewCreator.jsx
+++ src/pages/ViewCreator.jsx
@@ -1,7 +1,30 @@
-
+import { supabase } from "../client"
+import { useParams } from 'react-router-dom'
+import { useState } from "react"
+import { useEffect } from "react"
 
 function ViewCreator() {
-  return <h1>View Creators</h1>
+
+  const { id } = useParams()
+  const [creator, setCreator] = useState(null)
+
+  useEffect(() => {
+    async function getCreator() {
+      const { data } = await supabase.from('creators').select('*').eq('id', id).single()
+      setCreator(data)
+    }
+    getCreator()
+  }, [id])
+
+  if (!creator) return <h1>Loading...</h1>
+
+  return (
+    <div>
+        <h1>{creator.name}</h1>
+        <p>{creator.description}</p>
+        <a href={creator.url} target="_blank" rel="noopener noreferrer">{creator.url}</a>
+    </div>
+  )
 }
 
 export default ViewCreator
